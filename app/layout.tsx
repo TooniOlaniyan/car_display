4ca92ecb505a443559b35a2714484a4036918964
TooniOlaniyan <asalamlatif@gmail.com>
2023-11-15 21:26:35 +0100

    layout change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,26 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 
-import './globals.css'
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-
+import "./globals.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export const metadata: Metadata = {
-  title: 'Car Showcase',
-  description: 'Showcase all kinds of vehicle you have for sale',
-}
+  title: "Car Showcase",
+  description: "Showcase all kinds of vehicle you have for sale",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <div>
-        <Header/>
-        <body>{children}</body>
-        <Footer/>
-      </div>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
