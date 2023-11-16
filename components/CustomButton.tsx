af46a35f692c8c33eea0633d7f72ebb1bc8d2be5
TooniOlaniyan <asalamlatif@gmail.com>
2023-11-16 23:35:27 +0100

    new

---
+++ components/CustomButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { CustomBottonProps } from "@/types";
+
+const CustomButton = ({title , handleClick , containerStyles , btnType} : CustomBottonProps ) => {
+  return (
+    <button
+      type={btnType || 'button'}
+      disabled={false}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+        <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
---
+++ components/CustomFilters.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+type Props = {
+    title:string
+}
+
+const CustomFilters = ({title}: Props) => {
+  return (
+    <div>CustomFilters</div>
+  )
+}
+
+export default CustomFilters
